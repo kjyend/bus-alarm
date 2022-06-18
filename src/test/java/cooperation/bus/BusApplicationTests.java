1215fe3decc8a709afe4d64d68d99e3696c18415
junyoung kim <adad568@naver.com>
2022-06-18 21:42:53 +0900

    Initial commit

---
+++ src/test/java/cooperation/bus/BusApplicationTests.java
@@ -0,0 +1,13 @@
+package cooperation.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
